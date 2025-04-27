95ca3a0c058c3d68aba5b82db325e99e84887cb1
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-04-27 10:03:54 -0500

    discover solution via brute force enumeraiton

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
-
+import qualified Data.Set as Set
 -- some names so that every assignment can have some symbol.
 -- obviously quite constrained for this toy.
-data Var = A | B | C | D | E | F | G deriving (Show, Eq)
+data Var = A | B | C | D | E | F | G deriving (Show, Eq, Ord)
 
 data Value = Value Var Bool deriving (Show, Eq)
 
@@ -17,17 +17,23 @@ data Problem = Problem [Clause] deriving (Show, Eq)
 -- whether it satisfies the problem or not.
 data Assignment = Assignment [Value] deriving (Show, Eq)
 
+varSet :: Problem -> Set.Set Var
+varSet (Problem clauses) = (Set.fromList (map (\(Value v _) -> v) (concatMap (\(Clause values) -> values) clauses)))
+
 satisfiesValue :: Assignment -> Value -> Bool
 satisfiesValue (Assignment candidateValues) (Value name v) = any (\(Value name' v') -> name == name' && v == v') candidateValues 
 
 satisfiesClause :: Assignment -> Clause ->  Bool
 satisfiesClause assignment (Clause values) = any (satisfiesValue assignment) values
 
-satisfies :: Assignment -> Problem -> Bool
-satisfies assignment (Problem clauses) = all (satisfiesClause assignment) clauses
+satisfies :: Problem -> Assignment -> Bool
+satisfies (Problem clauses) assignment = all (satisfiesClause assignment) clauses
 
 enumerateAssignments :: [Var] -> [Assignment]
 enumerateAssignments vars = map Assignment (sequence [[Value v True, Value v False] | v <- vars])
+
+solve :: Problem -> [Assignment]
+solve problem = filter (satisfies problem) (enumerateAssignments (Set.toList (varSet problem)))
 
 main :: IO ()
 main = do
@@ -39,7 +45,9 @@ main = do
     putStrLn(show(problem))
     let a1 = Assignment [Value A True, Value B True, Value C True]
     let a2 = Assignment [Value A True, Value B True, Value C False]
-    let a1Solve = satisfies a1 problem
-    let a2Solve = satisfies a2 problem
+    let a1Solve = satisfies problem a1
+    let a2Solve = satisfies problem a2
     putStrLn(show(a1Solve))
     putStrLn(show(a2Solve))
+    let solution = (head (take 1 (solve problem)))
+    putStrLn(show(solution))
